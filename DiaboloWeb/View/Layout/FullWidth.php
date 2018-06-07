17c7e33e6c6204517894daf31eb0c05544aaca4f
Michael Luthor <michaelluthor@163.com>
2018-06-07 16:25:58 +0800

    update ui

--- DiaboloWeb/View/Layout/FullWidth.php
+++ DiaboloWeb/View/Layout/FullWidth.php
@@ -6,8 +6,7 @@ $links = $this->getLinkManager();
 $links->addCSS('bootstrap', '/assets/vendor/bootstrap/css/bootstrap.min.css');
 $links->addCSS('font-awesome', '/assets/vendor/font-awesome/css/font-awesome.min.css');
 $links->addCSS('simple-line-icons', '/assets/vendor/simple-line-icons/css/simple-line-icons.css');
-$links->addCSS('new-age', '/assets/css/new-age.min.css');
-$links->addCSS('new-age', '/assets/css/new-age.min.css');
+$links->addCSS('new-age', '/assets/css/new-age.css');
 
 $scripts = $this->getScriptManager();
 $scripts->add('jquery', '/assets/vendor/jquery/jquery.min.js');
--- DiaboloWeb/View/Particle/Index.php
+++ DiaboloWeb/View/Particle/Index.php
@@ -1,3 +1,6 @@
+<style>
+.guide-cmd {padding: 15px 0px;font-size: 1.3em;color: #8c8c8c;background-color: #dcdbdb;border-radius: 50px;box-shadow: 10px 10px 5px #888888;    margin: 25px;}
+</style>
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
   <div class="container">
     <a class="navbar-brand js-scroll-trigger" href="#page-top">
@@ -11,10 +14,10 @@
     <div class="collapse navbar-collapse" id="navbarResponsive">
       <ul class="navbar-nav ml-auto">
         <li class="nav-item">
-          <a class="nav-link js-scroll-trigger" href="#download">Guide</a>
+          <a class="nav-link js-scroll-trigger" href="#guide">Guide</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link js-scroll-trigger" href="#features">Document</a>
+          <a class="nav-link js-scroll-trigger" href="#document">Document</a>
         </li>
         <li class="nav-item">
           <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
@@ -41,7 +44,6 @@
                 <img src="/assets/img/big-logo.png" class="img-fluid" alt="">
               </div>
               <div class="button">
-                <!-- You can hook the "home button" to some JavaScript events or just remove it -->
               </div>
             </div>
           </div>
@@ -51,132 +53,106 @@
   </div>
 </header>
 
-    <section class="download bg-primary text-center" id="download">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8 mx-auto">
-            <h2 class="section-heading">Discover what all the buzz is about!</h2>
-            <p>Our app is available on any mobile device! Download now to get started!</p>
-            <div class="badges">
-              <a class="badge-link" href="#"><img src="/assets/img/google-play-badge.svg" alt=""></a>
-              <a class="badge-link" href="#"><img src="/assets/img/app-store-badge.svg" alt=""></a>
-            </div>
-          </div>
-        </div>
+<section class="download bg-primary text-center" id="guide">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 mx-auto">
+        <h2 class="section-heading">setup the application</h2>
+        <div class="guide-cmd"> composer create-project diabolo/diabolo-application </div>
+        <p>
+          <br><br>
+          <a href="#document" style="color:white">
+          <i class="icon-arrow-right"></i>
+          Go to manual
+         </a>
+        </p>
       </div>
-    </section>
+    </div>
+  </div>
+</section>
 
-    <section class="features" id="features">
-      <div class="container">
-        <div class="section-heading text-center">
-          <h2>Unlimited Features, Unlimited Fun</h2>
-          <p class="text-muted">Check out what you can do with this app theme!</p>
-          <hr>
-        </div>
-        <div class="row">
-          <div class="col-lg-4 my-auto">
-            <div class="device-container">
-              <div class="device-mockup iphone6_plus portrait white">
-                <div class="device">
-                  <div class="screen">
-                    <!-- Demo image for screen mockup, you can put an image here, some HTML, an animation, video, or anything else! -->
-                    <img src="/assets/img/demo-screen-1.jpg" class="img-fluid" alt="">
-                  </div>
-                  <div class="button">
-                    <!-- You can hook the "home button" to some JavaScript events or just remove it -->
-                  </div>
-                </div>
+<section class="features" id="document">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4 my-auto">
+        <div class="device-container">
+          <div class="device-mockup iphone6_plus portrait white">
+            <div class="device">
+              <div class="screen">
+                <!-- Demo image for screen mockup, you can put an image here, some HTML, an animation, video, or anything else! -->
+                <img src="/assets/img/big-logo.png" class="img-fluid" alt="">
               </div>
-            </div>
-          </div>
-          <div class="col-lg-8 my-auto">
-            <div class="container-fluid">
-              <div class="row">
-                <div class="col-lg-6">
-                  <div class="feature-item">
-                    <i class="icon-screen-smartphone text-primary"></i>
-                    <h3>Device Mockups</h3>
-                    <p class="text-muted">Ready to use HTML/CSS device mockups, no Photoshop required!</p>
-                  </div>
-                </div>
-                <div class="col-lg-6">
-                  <div class="feature-item">
-                    <i class="icon-camera text-primary"></i>
-                    <h3>Flexible Use</h3>
-                    <p class="text-muted">Put an image, video, animation, or anything else in the screen!</p>
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-lg-6">
-                  <div class="feature-item">
-                    <i class="icon-present text-primary"></i>
-                    <h3>Free to Use</h3>
-                    <p class="text-muted">As always, this theme is free to download and use for any purpose!</p>
-                  </div>
-                </div>
-                <div class="col-lg-6">
-                  <div class="feature-item">
-                    <i class="icon-lock-open text-primary"></i>
-                    <h3>Open Source</h3>
-                    <p class="text-muted">Since this theme is MIT licensed, you can use it commercially!</p>
-                  </div>
-                </div>
+              <div class="button">
+                <!-- You can hook the "home button" to some JavaScript events or just remove it -->
               </div>
             </div>
           </div>
         </div>
       </div>
-    </section>
-
-    <section class="cta">
-      <div class="cta-content">
-        <div class="container">
-          <h2>Stop waiting.<br>Start building.</h2>
-          <a href="#contact" class="btn btn-outline btn-xl js-scroll-trigger">Let's Get Started!</a>
+      <div class="col-lg-8 my-auto">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-lg-6">
+              <div class="feature-item">
+                <i class="icon-book-open text-primary"></i>
+                <h3>Manual</h3>
+                <p class="text-muted">Start to build your application step by step !</p>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="feature-item">
+                <i class="icon-list text-primary"></i>
+                <h3>Class Reference</h3>
+                <p class="text-muted">Find out how the class works !</p>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-lg-6">
+              <div class="feature-item">
+                <i class="icon-present text-primary"></i>
+                <h3>Extensions</h3>
+                <p class="text-muted">A lot of extensions to save your time !</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div class="overlay"></div>
-    </section>
+    </div>
+  </div>
+</section>
 
-    <section class="contact bg-primary" id="contact">
-      <div class="container">
-        <h2>We
-          <i class="fa fa-heart"></i>
-          new friends!</h2>
-        <ul class="list-inline list-social">
-          <li class="list-inline-item social-twitter">
-            <a href="#">
-              <i class="fa fa-twitter"></i>
-            </a>
-          </li>
-          <li class="list-inline-item social-facebook">
-            <a href="#">
-              <i class="fa fa-facebook"></i>
-            </a>
-          </li>
-          <li class="list-inline-item social-google-plus">
-            <a href="#">
-              <i class="fa fa-google-plus"></i>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </section>
+<section class="contact bg-primary" id="contact">
+  <div class="container">
+    <h2>We <i class="fa fa-heart"></i> new friends!</h2>
+    <ul class="list-inline list-social">
+      <li class="list-inline-item social-github">
+        <a href="https://github.com/Diabolo-Framework/Diabolo">
+          <i class="fa fa-github"></i>
+        </a>
+      </li>
+      <li class="list-inline-item social-mail">
+        <a href="mailto:michaelluthor@163.com">
+          <i class="fa fa-envelope"></i>
+        </a>
+      </li>
+    </ul>
+  </div>
+</section>
 
-    <footer>
-      <div class="container">
-        <p>&copy; Your Website 2018. All Rights Reserved.</p>
-        <ul class="list-inline">
-          <li class="list-inline-item">
-            <a href="#">Privacy</a>
-          </li>
-          <li class="list-inline-item">
-            <a href="#">Terms</a>
-          </li>
-          <li class="list-inline-item">
-            <a href="#">FAQ</a>
-          </li>
-        </ul>
-      </div>
-    </footer>
+<footer>
+  <div class="container">
+    <p>&copy; Diabolo Framework <?php echo date('Y'); ?>. All Rights Reserved.</p>
+    <ul class="list-inline">
+      <li class="list-inline-item">
+        <a href="javascript:;">Privacy</a>
+      </li>
+      <li class="list-inline-item">
+        <a href="javascript:;">Terms</a>
+      </li>
+      <li class="list-inline-item">
+        <a href="javascript:;">FAQ</a>
+      </li>
+    </ul>
+  </div>
+</footer>
